227c28ac90c8a2f80805010cf24ba92fc4fc830e
Felipefreitas020 <felipefreitasreboucas@gmail.com>
2025-05-23 07:20:46 -0300

    Add files via upload

---
+++ 06-05/06-05/src/App.jsx
@@ -0,0 +1,6 @@
+export default function App() {
+  return (
+    <div>
+      <h1>Usando Props com Rest Params</h1>
+
+      <Botao nome='John' idade='34' />
---
+++ 06-05/components/botao/index.jsx
@@ -0,0 +1,10 @@
+import './style.css'
+
+export default function Botao({ nome, idade }) {
+    return (
+        <button className="botao-container">
+            {nome} {idade}
+        </button>
+    )
+}
+}
